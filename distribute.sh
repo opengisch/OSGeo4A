46acade3fce944c2d6f1cca179e7e8d0f3319305
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-11-29 20:17:19 +0100

    Add stl include and lib directories

--- distribute.sh
+++ distribute.sh
@@ -181,10 +181,7 @@ function push_arm() {
 	if [ "X$ARCH" == "Xarmeabi-v7a" ]; then
 		CFLAGS+=" -march=armv7-a -mfloat-abi=softfp -mfpu=vfp -mthumb"
 	fi
-	export CXXFLAGS="$CFLAGS"
 
-	# that could be done only for darwin platform, but it doesn't hurt.
-	export LDFLAGS="-lm -L$DIST_PATH/lib"
 
 	# this must be something depending of the API level of Android
 	PYPLATFORM=$($PYTHON -c 'from __future__ import print_function; import sys; print(sys.platform)')
@@ -194,22 +191,26 @@ function push_arm() {
 		PYPLATFORM="linux"
 	fi
 
-    if [ "X$ANDROIDNDKVER" == "Xr5b" ]; then
-        export TOOLCHAIN_PREFIX=arm-eabi
-        export TOOLCHAIN_VERSION=4.4.0
-    elif [ "X${ANDROIDNDKVER:0:2}" == "Xr7" ] || [ "X${ANDROIDNDKVER:0:2}" == "Xr8" ]; then
-        export TOOLCHAIN_PREFIX=arm-linux-androideabi
-        export TOOLCHAIN_VERSION=4.4.3
-    elif  [ "X${ANDROIDNDKVER:0:2}" == "Xr9" ]; then
-        export TOOLCHAIN_PREFIX=arm-linux-androideabi
-        export TOOLCHAIN_VERSION=4.8
-    elif [ "X${ANDROIDNDKVER:0:3}" == "Xr10" ]; then
-        export TOOLCHAIN_PREFIX=arm-linux-androideabi
-        export TOOLCHAIN_VERSION=4.9
-    else
-        echo "Error: Please report issue to enable support for newer ndk."
-        exit 1
-    fi
+  if [ "X$ANDROIDNDKVER" == "Xr5b" ]; then
+      export TOOLCHAIN_PREFIX=arm-eabi
+      export TOOLCHAIN_VERSION=4.4.0
+  elif [ "X${ANDROIDNDKVER:0:2}" == "Xr7" ] || [ "X${ANDROIDNDKVER:0:2}" == "Xr8" ]; then
+      export TOOLCHAIN_PREFIX=arm-linux-androideabi
+      export TOOLCHAIN_VERSION=4.4.3
+  elif  [ "X${ANDROIDNDKVER:0:2}" == "Xr9" ]; then
+      export TOOLCHAIN_PREFIX=arm-linux-androideabi
+      export TOOLCHAIN_VERSION=4.8
+  elif [ "X${ANDROIDNDKVER:0:3}" == "Xr10" ]; then
+      export TOOLCHAIN_PREFIX=arm-linux-androideabi
+      export TOOLCHAIN_VERSION=4.9
+  else
+      echo "Error: Please report issue to enable support for newer ndk."
+      exit 1
+  fi
+
+	export CXXFLAGS="$CFLAGS -isystem $ANDROIDNDK/sources/cxx-stl/gnu-libstdc++/$TOOLCHAIN_VERSION/include -isystem $ANDROIDNDK/sources/cxx-stl/gnu-libstdc++/$TOOLCHAIN_VERSION/libs/${ARCH}/include"
+
+	export LDFLAGS="-lm -L$DIST_PATH/lib -L$ANDROIDNDK/sources/cxx-stl/gnu-libstdc++/$TOOLCHAIN_VERSION/libs/${ARCH}"
 
 	export PATH="$DIST_PATH/bin:$ANDROIDNDK/toolchains/$TOOLCHAIN_PREFIX-$TOOLCHAIN_VERSION/prebuilt/$PYPLATFORM-x86/bin/:$ANDROIDNDK/toolchains/$TOOLCHAIN_PREFIX-$TOOLCHAIN_VERSION/prebuilt/$PYPLATFORM-x86_64/bin/:$ANDROIDNDK:$ANDROIDSDK/tools:$QTSDK/android_armv7/bin:$PATH"
 
