00db7c47315c91c279a7dacb4a0be53ed24f8b19
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-03-12 08:29:24 +0100

    enable gdal realloc debug info (not working yet)

--- recipes/gdal/recipe.sh
+++ recipes/gdal/recipe.sh
@@ -31,6 +31,7 @@ function prebuild_gdal() {
   try cp $BUILD_PATH/tmp/config.sub $BUILD_gdal
   try cp $BUILD_PATH/tmp/config.guess $BUILD_gdal
   try patch -p1 < $RECIPE_gdal/patches/gdal.patch
+  try patch -p1 < $RECIPE_gdal/patches/memdebug.patch
 
   touch .patched
 }
