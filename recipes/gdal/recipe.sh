71c181a0774047393e32643f44fb0d39210ca661
Matthias Kuhn <matthias@opengis.ch>
2016-09-03 08:21:32 +0200

    Update GDAL to 2.1.1

--- recipes/gdal/recipe.sh
+++ recipes/gdal/recipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version of your package
-VERSION_gdal=2.1.0
+VERSION_gdal=2.1.1
 
 # dependencies of this recipe
 DEPS_gdal=(iconv sqlite3 geos libtiff postgresql)
@@ -10,7 +10,7 @@ DEPS_gdal=(iconv sqlite3 geos libtiff postgresql)
 URL_gdal=http://download.osgeo.org/gdal/$VERSION_gdal/gdal-${VERSION_gdal}.tar.gz
 
 # md5 of the package
-MD5_gdal=0fc165cd947c54b132204233dfb243f1
+MD5_gdal=2a7037c5b30063cecfa104a562c9c07e
 
 # default build path
 BUILD_gdal=$BUILD_PATH/gdal/$(get_directory $URL_gdal)
