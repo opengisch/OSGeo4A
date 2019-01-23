d919d190e5046e6c425028565b829796fa314a04
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-01-23 16:21:49 +0100

    Revert "update qgis"
    This reverts commit 38f688164bb0554c25a7df0a412eff87ac902e40.

--- recipes/qgis/recipe.sh
+++ recipes/qgis/recipe.sh
@@ -8,10 +8,10 @@ DEPS_qgis=(gdal qca libspatialite spatialindex expat gsl postgresql libzip qtkey
 # DEPS_qgis=()
 
 # url of the package
-URL_qgis=https://github.com/3nids/QGIS/archive/b391c0876fee2dd1161dcf9136f097b96d2cd822.zip
+URL_qgis=https://github.com/3nids/QGIS/archive/9e5bb360f9b15150f239aed6e9b5cd0a98202344.zip
 
 # md5 of the package
-MD5_qgis=33b3d070fcb70113e437d47928b2f6b2
+MD5_qgis=39c46efbcb58e8f3bcb4da50b2f1104f
 
 # default build path
 BUILD_qgis=$BUILD_PATH/qgis/$(get_directory $URL_qgis)
