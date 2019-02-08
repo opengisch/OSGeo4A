cc7d2e8639e0d5d4321dbfd9a246be86d236073a
Matthias Kuhn <matthias@opengis.ch>
2019-02-08 22:04:59 +0100

    Bump QCA to master for openssl 1.1 compat

--- recipes/qca/recipe.sh
+++ recipes/qca/recipe.sh
@@ -9,10 +9,11 @@ DEPS_qca=(openssl)
 # url of the package
 # URL_qca=http://delta.affinix.com/download/qca/2.0/qca-${VERSION_qca}.tar.gz
 # URL_qca=http://quickgit.kde.org/?p=qca.git&a=snapshot&h=4f966b0217c10b6fd3c12caf7d2467759fbec7f7&fmt=tgz
-URL_qca=https://github.com/KDE/qca/archive/v${VERSION_qca}.tar.gz
+#URL_qca=https://github.com/KDE/qca/archive/v${VERSION_qca}.tar.gz
+URL_qca=https://github.com/KDE/qca/archive/32343842d359a60e3619f97aac983d587f6eca16.zip
 
 # md5 of the package
-MD5_qca=bd646d08fdc1d9be63331a836ecd528f
+MD5_qca=180212f7d8d9290b4eca19717b4809f6
 
 # default build path
 BUILD_qca=$BUILD_PATH/qca/$(get_directory $URL_qca)
