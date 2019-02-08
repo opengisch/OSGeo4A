bed638104b7ad7d1eeafa55a7bcbb2fe1c245ec2
Matthias Kuhn <matthias@opengis.ch>
2019-02-08 08:53:20 +0100

    Bump openssl to version 1.1.1a

--- recipes/openssl/recipe.sh
+++ recipes/openssl/recipe.sh
@@ -4,7 +4,7 @@
 # To see how to build OpenSSL 1.1.x, check  https://github.com/opengisch/OSGeo4A/pull/25
 
 # version of your package
-VERSION_openssl=1.0.2o
+VERSION_openssl=1.1.1a
 
 # dependencies of this recipe
 DEPS_openssl=()
@@ -28,12 +28,6 @@ function prebuild_openssl() {
     return
   fi
 
-  echo "patching $BUILD_PATH/openssl/openssl-${VERSION_openssl}/config"
-  try $SED 's/armv\[7-9\]\*-\*-android/armeabi-v7a\*-\*-android|armv\[7-9\]\*-\*-android/g' $BUILD_PATH/openssl/openssl-${VERSION_openssl}/config
-
-  echo "patching $BUILD_PATH/openssl/openssl-${VERSION_openssl}/Configure"
-  LC_ALL=C try $SED 's/SHLIB_EXT=$shared_extension/SHLIB_EXT=.so/g' $BUILD_PATH/openssl/openssl-${VERSION_openssl}/Configure
-
   touch .patched
 }
 
@@ -46,55 +40,17 @@ function shouldbuild_openssl() {
 
 # function called to build the source code
 function build_openssl() {
-  # unfortunately config and Configure uses relative paths to this
-  # se we need to do in-source build
-  try cp -r $BUILD_openssl $BUILD_PATH/openssl/build-$ARCH
+  try mkdir -p $BUILD_PATH/openssl/build-$ARCH
   try cd $BUILD_PATH/openssl/build-$ARCH
 
   push_arm
-
-  # https://wiki.openssl.org/index.php/Android
-  export _ANDROID_ARCH=arch-$SHORTARCH
-  export _ANDROID_EABI=$TOOLCHAIN_PREFIX-$TOOLCHAIN_VERSION
-  export _ANDROID_NDK=$ANDROIDNDK
-  export _ANDROID_NDK_ROOT=$ANDROIDNDK
-  export ANDROID_SDK_ROOT=$ANDROIDSDK
-  export _ANDROID_API=android-$ANDROIDAPI
-
-  export MACHINE=${ARCH}
-  export RELEASE=2.6.37
-  export SYSTEM=android
-  # export ARCH is exported in distribute.sh
-
-  export ANDROID_DEV=$_ANDROID_NDK/platforms/$_ANDROID_API/$_ANDROID_ARCH/usr
-  export HOSTCC=gcc
-  export ANDROID_TOOLCHAIN=$ANDROIDNDK/toolchains/$TOOLCHAIN_BASEDIR-$TOOLCHAIN_VERSION/prebuilt/$PYPLATFORM-x86_64/bin
-
-  CFLAGS_WITHOUT_QUOTES=$(eval echo $CFLAGS)
-  CFLAGS_WITHOUT_SYSROOT=${CFLAGS_WITHOUT_QUOTES//"--sysroot $NDKPLATFORM"/}
-
-  ./config \
-      shared no-hw \
-      --openssldir=/usr/local/ssl/$ANDROIDAPI/ \
-      -D__ANDROID_API__=$ANDROIDAPI \
-      --prefix=/  \
-      $(eval echo $CFLAGS_WITHOUT_SYSROOT)
-
-  echo "patching openssl-${VERSION_openssl}/Makefile"
-  # remove docs
-  try $SED '646,690d' Makefile
-  try $SED '621,643d' Makefile
-  # remove "link-shared" target, since we do not want links
-  try $SED '346,352d' Makefile
-  # remove so.x.y versions
-  try $SED 's/LIBVERSION=$(SHLIB_MAJOR).$(SHLIB_MINOR)//g' Makefile
-  try $SED 's/LIBCOMPATVERSIONS=";$(SHLIB_VERSION_HISTORY)"//g' Makefile
-
-  ${MAKESMP} depend
-  ${MAKESMP} CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" build_libs
-
-  # install
-  try ${MAKE} INSTALL_PREFIX=$STAGE_PATH install
+  try $BUILD_openssl/Configure \
+    android-arm \
+    no-asm \
+    --prefix=$STAGE_PATH \
+    -D__ANDROID_API__=21
+  try $MAKESMP
+  try make install &> install.log
     
   pop_arm
 }
