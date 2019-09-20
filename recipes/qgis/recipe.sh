6d4507424570259fabc0459bbed7bedcf413022d
Matthias Kuhn <matthias@opengis.ch>
2019-09-20 18:13:42 +0200

    Bump QGIS version

--- recipes/qgis/recipe.sh
+++ recipes/qgis/recipe.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # version of your package
-VERSION_qgis=3.7
+VERSION_qgis=3.9
 
 # dependencies of this recipe
 DEPS_qgis=(zlib gdal qca libspatialite libspatialindex expat gsl postgresql libzip qtkeychain exiv2)
 # DEPS_qgis=()
 
 # url of the package
-URL_qgis=https://github.com/qgis/QGIS/archive/1570e49b0b72552f3daaa4ead84ba92eef3d4b52.tar.gz
+URL_qgis=https://github.com/qgis/QGIS/archive/387bffe20eaa3a774d51acf9d943855a9a0ecb01.tar.gz
 
 # md5 of the package
-MD5_qgis=2ccce6c0461c8c845bc04ea02e4004ab
+MD5_qgis=13de739049fb4d06619b72f1b354349e
 
 # default build path
 BUILD_qgis=$BUILD_PATH/qgis/$(get_directory $URL_qgis)
