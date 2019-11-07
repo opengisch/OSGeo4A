4f8f8bd97ae396fdce9bd25383a515d204dc3c4d
Matthias Kuhn <matthias@opengis.ch>
2019-11-07 13:49:12 +0100

    Fix multiarch build

--- scripts/build_arches.sh
+++ scripts/build_arches.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+mkdir -p /home/osgeo4a
 IFS=' ' read -ra arches_array <<< "${ARCHES}"
 for ARCH in "${arches_array[@]}"
 do
 	export ARCH=$ARCH
 	/usr/src/distribute.sh -m qgis
+	mv /usr/src/build/stage/$ARCH /home/osgeo4a
 done
-cp -r /usr/src/build/stage/* /home/osgeo4a
 rm -rf /usr/src
