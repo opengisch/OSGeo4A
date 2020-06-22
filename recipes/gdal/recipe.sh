98f9292768da3337b97d3e9b39dfbe8561bc7d0f
Matthias Kuhn <matthias@opengis.ch>
2020-06-22 22:42:04 +0200

    Bump to gdal 3.1.0 and proj 6.3.2

--- recipes/gdal/recipe.sh
+++ recipes/gdal/recipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # version of your package
-VERSION_gdal=3.0.1
+VERSION_gdal=3.1.0
 
 # dependencies of this recipe
 DEPS_gdal=(iconv sqlite3 geos libtiff postgresql expat zlib openjpeg libspatialite webp)
@@ -10,7 +10,7 @@ DEPS_gdal=(iconv sqlite3 geos libtiff postgresql expat zlib openjpeg libspatiali
 URL_gdal=http://download.osgeo.org/gdal/$VERSION_gdal/gdal-${VERSION_gdal}.tar.gz
 
 # md5 of the package
-MD5_gdal=2b397c041e6b0b10ec7c49fd76e9fa99
+MD5_gdal=bda0f002cd63b51c8d2f1b1400daffa9
 
 # default build path
 BUILD_gdal=$BUILD_PATH/gdal/$(get_directory $URL_gdal)
@@ -32,7 +32,6 @@ function prebuild_gdal() {
   try cp $ROOT_OUT_PATH/.packages/config.guess $BUILD_gdal
   # Remove bundled lib
   try rm -rf $BUILD_gdal/frmts/zlib
-  try patch -p1 < $RECIPE_gdal/patches/gdal-zlib.patch
 
   touch .patched
 }
