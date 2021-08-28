6bdc3803f17eff4729b883b715a6a085d705e06d
nirvn <nirvn.asia@gmail.com>
2021-08-28 12:56:14 +0700

    Let's jump back onto an official exiv2 release if possible

--- recipes/exiv2/recipe.sh
+++ recipes/exiv2/recipe.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # version of your package
-VERSION_exiv2=0.27
+VERSION_exiv2=0.27.4
 
 # dependencies of this recipe
 DEPS_exiv2=(expat iconv)
 
 # url of the package
-#URL_exiv2=http://www.exiv2.org/builds/exiv2-0.26-trunk.tar.gz
-URL_exiv2=https://github.com/Exiv2/exiv2/archive/d8a15e1d132e260dd333ebc5af1e6929257a6d0f.zip
+URL_exiv2=https://github.com/Exiv2/exiv2/archive/refs/tags/v${VERSION_exiv2}.tar.gz
 
 # md5 of the package
-MD5_exiv2=7a423fabb9bfbdaecbcaa24db1c5abd3
+MD5_exiv2=ecabc1459a7d70f91799735ed7d8a80c
 
 # default build path
 BUILD_exiv2=$BUILD_PATH/exiv2/$(get_directory $URL_exiv2)
